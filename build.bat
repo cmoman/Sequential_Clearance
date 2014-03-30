a7f7831221455c7f635860643fa68874b0e772c4
Chris <chris@blahdeblah.co.nz>
2014-03-31 09:36:52 +1300

    Tidy up.

--- build.bat
+++
@@ -1,3 +0,0 @@
-C:\Python27\Lib\site-packages\PyQt4\pyuic4.bat -x sequential_clearance_mainwindow.ui -o ui_sequential_clearance_mainwindow.py
-C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe sequential_clearance_resources.qrc -o sequential_clearance_resources.py
-C:\Python27\Lib\site-packages\PyQt4\pyrcc4.exe sequential_clearance_resources2.qrc -o sequential_clearance_resources2.py
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-pyuic4 sequential_clearance_mainwindow.ui -o ui_sequential_clearance_mainwindow.py
-pyrcc4 sequential_clearance_resources.qrc -o sequential_clearance_resources_rc.qrc
-pyrcc4 sequential_clearance_resources2.qrc -o sequential_clearance_resources2_rc.qrc
